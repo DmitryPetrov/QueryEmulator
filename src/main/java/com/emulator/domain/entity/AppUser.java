6352d9cda1641f2731624ec3671e3848782a09d1
Dmitry Petrov <dmitrypetrov93gmail.com>
2019-07-19 15:59:03 +0300

    add: sessionId in AppUser

--- src/main/java/com/emulator/domain/entity/AppUser.java
+++ src/main/java/com/emulator/domain/entity/AppUser.java
@@ -1,9 +1,12 @@
 package com.emulator.domain.entity;
 
+import com.emulator.domain.soap.login.LoginResult;
+
 public class AppUser {
 
     private String userName;
     private String password;
+    private String sessionId;
 
     public AppUser(String name, String password) {
         this.userName = name;
@@ -24,5 +27,17 @@ public class AppUser {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public void setSessionId(LoginResult loginResult) {
+        this.sessionId = loginResult.getSessionId();
     }
 }
