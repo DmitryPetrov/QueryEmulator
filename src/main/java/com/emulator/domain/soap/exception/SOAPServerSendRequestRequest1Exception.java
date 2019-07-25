fc24f423828e197d2b045fd06a6de1d2f4bef906
Dmitry Petrov <dmitrypetrov93gmail.com>
2019-07-25 12:19:57 +0300

    add: exception for sendRequest request1

---
+++ src/main/java/com/emulator/domain/soap/exception/SOAPServerSendRequestRequest1Exception.java
@@ -0,0 +1,9 @@
+package com.emulator.domain.soap.exception;
+
+public class SOAPServerSendRequestRequest1Exception extends SOAPServerBadResponseException {
+
+    public SOAPServerSendRequestRequest1Exception(String message) {
+        super(message);
+    }
+
+}
