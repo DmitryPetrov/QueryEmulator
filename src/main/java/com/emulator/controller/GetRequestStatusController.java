4791b392c6f3d87eb83ef6f25f145fddb1153ae7
Dmitry Petrov <dmitrypetrov93gmail.com>
2019-08-09 15:28:51 +0300

    add: mark 'GetRequestStatusController' as controller [RU - пометил класс 'GetRequestStatusController' как controller]

--- src/main/java/com/emulator/controller/GetRequestStatusController.java
+++ src/main/java/com/emulator/controller/GetRequestStatusController.java
@@ -6,12 +6,14 @@ import com.emulator.domain.soap.SOAPClient;
 import com.emulator.domain.soap.exception.SOAPServerBadResponseException;
 import com.emulator.domain.soap.exception.SOAPServerGetRequestStatusException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 
+@Controller
 public class GetRequestStatusController extends AbstractController {
 
     @Autowired
