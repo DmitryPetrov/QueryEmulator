df8d9fbb4cfc577c876d7c56a5f1e639e4f42bea
Dmitry Petrov <dmitrypetrov93gmail.com>
2019-08-12 14:45:00 +0300

    update: GetRequestStatusController

--- src/main/java/com/emulator/controller/GetRequestStatusController.java
+++ src/main/java/com/emulator/controller/GetRequestStatusController.java
@@ -1,17 +1,22 @@
 package com.emulator.controller;
 
 import com.emulator.domain.entity.AppUser;
-import com.emulator.domain.frontend.SOAPConnectionStatus;
+import com.emulator.domain.entity.RequestParameters;
+import com.emulator.domain.frontend.response.ResponseBodyData;
+import com.emulator.domain.frontend.response.ResponseBodySOAPRequestStatus;
 import com.emulator.domain.soap.SOAPClient;
-import com.emulator.domain.soap.exception.SOAPServerBadResponseException;
-import com.emulator.domain.soap.exception.SOAPServerGetRequestStatusException;
+import com.emulator.exception.BadRequestParameterException;
+import com.emulator.exception.SOAPServerBadResponseException;
+import com.emulator.exception.SOAPServerGetRequestStatusException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 public class GetRequestStatusController extends AbstractController {
@@ -19,50 +24,72 @@ public class GetRequestStatusController extends AbstractController {
     @Autowired
     private SOAPClient soapClient;
 
-    @RequestMapping(value = "/sendRequests/getRequestStatus", method = RequestMethod.POST)
+    @RequestMapping(value = "/sendRequests/getRequestStatus", method = RequestMethod.GET)
     @ResponseBody
-    public SOAPConnectionStatus runGetRequestStatus(HttpSession httpSession) {
-        AppUser user = (AppUser) httpSession.getAttribute("user");
-        if ((user == null) || user.getSessionId().equals("")) {
-            return getUserIsNotAuthorizedResponse();
-        }
-
-        String requestId = (String) httpSession.getAttribute("requestId");
-        if((requestId == null) || (requestId.equals(""))) {
-            return getNotFoundRequestIdResponse();
-        }
-
+    public ResponseBodyData runGetRequestStatus(HttpSession httpSession,
+                                                @RequestParam(name = "requestId") String requestId) {
         try {
+            AppUser user = (AppUser) httpSession.getAttribute("user");
+            if (user == null) {
+                return getUserIsNotAuthorizedResponse();
+            }
+
+            checkRequestId(httpSession, requestId);
+
             soapClient.sendGetRequestStatus(user, requestId);
-            return getRequestSuccessResponse("requestId");
+            return getSOAPRequestSuccessResponse("requestId");
         } catch (SOAPServerGetRequestStatusException e) {
             e.printStackTrace();
-            return getRequestFailResponse(e);
+            return getSOAPRequestFailResponse(e);
+        } catch (BadRequestParameterException e) {
+            e.printStackTrace();
+            return getBadRequestParameterResponse(e);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return getServerFailResponse(e);
         }
     }
 
     @Override
-    protected SOAPConnectionStatus getRequestSuccessResponse(String message) {
-        SOAPConnectionStatus result = new SOAPConnectionStatus();
+    protected ResponseBodySOAPRequestStatus getSOAPRequestSuccessResponse(String message) {
+        ResponseBodySOAPRequestStatus result = new ResponseBodySOAPRequestStatus();
         result.setStatus("OK");
         result.setMessage("GetRequestStatus to SOAP server is success. requestID=" + message);
         return result;
     }
 
     @Override
-    protected SOAPConnectionStatus getRequestFailResponse(SOAPServerBadResponseException exception) {
-        SOAPConnectionStatus result = new SOAPConnectionStatus();
+    protected ResponseBodySOAPRequestStatus getSOAPRequestFailResponse(SOAPServerBadResponseException exception) {
+        ResponseBodySOAPRequestStatus result = new ResponseBodySOAPRequestStatus();
         result.setStatus("ERROR");
         result.setMessage("GetRequestStatus to SOAP server is fail. message=" + exception.getSoapResponse());
         result.setSoapMessages("<SoapMessages>" + exception.getSoapMessages() + "</SoapMessages>");
         return result;
     }
 
-    private SOAPConnectionStatus getNotFoundRequestIdResponse() {
-        SOAPConnectionStatus result = new SOAPConnectionStatus();
+    private ResponseBodyData getBadRequestParameterResponse(BadRequestParameterException e) {
+        ResponseBodyData result = new ResponseBodyData();
         result.setStatus("ERROR");
-        result.setMessage("Not found request id.");
+        result.setMessage("Parameter " + e.getParameterName() + " not found on server");
         return result;
+    }
+
+    private void checkRequestId(HttpSession httpSession, String requestId) {
+        boolean requestFound = false;
+
+        List<RequestParameters> requestList = (List<RequestParameters>) httpSession.getAttribute("requestList");
+        for (RequestParameters request: requestList) {
+            if (request.getRequestId().equals(requestId)){
+                requestFound = true;
+            }
+        }
+
+        if (!requestFound) {
+            String message = "Parameter not found on server";
+            BadRequestParameterException exception = new BadRequestParameterException(message);
+            exception.setParameterName("requestId");
+            throw exception;
+        }
     }
 
 }
