3547b68a619ffeedbda652ee5124cea468cf1363
Dmitry Petrov <dmitrypetrov93gmail.com>
2020-01-16 16:46:08 +0300

    remove: useless imports [scope:test */payrequest/MessageBuilderTest.java] [RU - удалил бесполезные импорты]

--- src/test/java/com/emulator/domain/soap/requests/payrequest/MessageBuilderTest.java
+++ src/test/java/com/emulator/domain/soap/requests/payrequest/MessageBuilderTest.java
@@ -7,16 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerFactory;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class MessageBuilderTest {
 
