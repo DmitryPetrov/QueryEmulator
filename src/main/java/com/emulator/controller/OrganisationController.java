5c6b961a0bbae4d50fa705408cadb3e56d09373c
Dmitry Petrov <dmitrypetrov93gmail.com>
2020-04-29 14:16:04 +0300

    fix: ViewResolver error [scope:class */controller/OrganisationController.java] [RU - исправил ошибку с ViewResolver]

--- src/main/java/com/emulator/controller/OrganisationController.java
+++ src/main/java/com/emulator/controller/OrganisationController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
-@Controller
+@RestController
 @RequestMapping("/organisations")
 public class OrganisationController {
 
