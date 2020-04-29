0c4bd89e9e8d3e34cdf8a1667dbb20951c3cf2ee
Dmitry Petrov <dmitrypetrov93gmail.com>
2020-04-29 14:18:41 +0300

    refactor: remove useless imports [scope:package */repository/*] [RU - удалил бесполезые импорты]

--- src/main/java/com/emulator/repository/OrganisationRepository.java
+++ src/main/java/com/emulator/repository/OrganisationRepository.java
@@ -1,7 +1,6 @@
 package com.emulator.repository;
 
 import com.emulator.repository.entity.Organisation;
-import org.hibernate.cfg.NotYetImplementedException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
